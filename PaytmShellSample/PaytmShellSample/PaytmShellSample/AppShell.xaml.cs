2211635316392474a666e1eea68d3cd43b92ec37
Dnyanesh <dnyan_war@yahoo.co.in>
2021-05-21 20:17:15 +0530

    Sample inception code

---
+++ PaytmShellSample/PaytmShellSample/PaytmShellSample/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using PaytmShellSample.ViewModels;
+using PaytmShellSample.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PaytmShellSample
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+			Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+		}
+
+	}
+}
---
+++ PaytmShellSample/PaytmShellSample/PaytmShellSample/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PaytmShellSample.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PaytmShellSample.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
